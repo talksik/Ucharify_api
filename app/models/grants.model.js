f90561af31f1816fd7299a279fd2fcbae4e407b0
Arjun Patel <patel.arjun50@gmail.com>
2019-01-13 23:37:38 -0800

    Update grants.model.js

--- app/models/grants.model.js
+++ app/models/grants.model.js
@@ -1,21 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-	const Campaigns = sequelize.define('campaigns', {
+	const Grants = sequelize.define('grants', {
 		id: {
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: true
 		},
-		// user_id: {
-		// 	type: DataTypes.INTEGER,
-		// 	references: {
-		// 		// This is a reference to another model
-		// 		model: Donors,
-		// 		// This is the column name of the referenced model
-		// 		key: 'id'
-		// 	}
-		// },
 		name: DataTypes.STRING,
-		amount: DataTypes.INTEGER
+		amount: DataTypes.INTEGER,
+		last_payment: DataTypes.DATE,
+		monthly: DataTypes.TINYINT(1),
+		num_causes: DataTypes.INTEGER,
+		num_regions: DataTypes.INTEGER
 	});
-	return Campaigns;
+	return Grants;
 };
