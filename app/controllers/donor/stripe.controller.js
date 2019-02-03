79112d9e48f9c8ad35ce920d64b1489c1a5b9888
Arjun Patel <patel.arjun50@gmail.com>
2019-02-02 22:59:35 -0800

    fixing bug for undefined

--- app/controllers/donor/stripe.controller.js
+++ app/controllers/donor/stripe.controller.js
@@ -92,7 +92,7 @@ exports.grantCharge = async (grant, req, res, next) => {
 
 		return res.status(200).json({
 			message: 'Successfully charged or subscribed',
-			grant: req.grant
+			grant
 		});
 	} catch (error) {
 		next(error);
